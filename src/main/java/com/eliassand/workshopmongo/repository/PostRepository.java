7ec42544e38c26b007726b64b2b0d1fa404f88ae
Elias Mathias Sand <104523320+EliasNote@users.noreply.github.com>
2024-04-17 09:53:10 -0300

    Criando entity Post com user aninhado

---
+++ src/main/java/com/eliassand/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.eliassand.workshopmongo.repository;
+
+import com.eliassand.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
